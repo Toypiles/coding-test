88112cee4e1e2b5261e4c0522b7cd9a2a91eabb6
Toypiles <gyeol3474@gmail.com>
2024-10-12 05:11:19 +0000

    2022-10-12.md update

--- java/src/App.java
+++ java/src/App.java
@@ -1,46 +1,24 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
+import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        for(int i=0; i<3; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            String[] startTime = st.nextToken().split(":");
-            String[] endTime = st.nextToken().split(":");
-            int h1 = Integer.valueOf(startTime[0]);
-            int m1 = Integer.valueOf(startTime[1]);
-            int s1 = Integer.valueOf(startTime[2]);
-            int h2 = Integer.valueOf(endTime[0]);
-            int m2 = Integer.valueOf(endTime[1]);
-            int s2 = Integer.valueOf(endTime[2]);
-            int result = 0;
-            while(true){
-                if(s1==60){
-                    s1=0;
-                    m1+=1;
-                }
-                if(m1==60){
-                    m1=0;
-                    h1+=1;
-                }
-                if(h1 == 24){
-                    h1=0;
-                }
-                int timeNumber = h1*10000 + m1*100 +s1;
-                if(timeNumber%3==0){
-                    result++;
-                }
-                if(h1==h2 && m1==m2 && s1==s2){
-                    break;
-                } else{
-                    s1++;
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for(int i=0; i<n; i++){
+            int p = scanner.nextInt();
+            scanner.nextLine();
+            String[] C = scanner.nextLine().split(" ");
+            int cmp = Integer.parseInt(C[0]);
+            String result = C[1];
+            for(int j=2; j<=p; j++){
+                String[] sp = scanner.nextLine().split(" ");
+                if(Integer.parseInt(sp[0])>cmp){
+                    cmp=Integer.parseInt(sp[0]);
+                    result = sp[1];
                 }
             }
             System.out.println(result);
         }
+        scanner.close();
     }
 }
