ca125f27e2e706618b95d71ac13fe370f1abbe70
Toypiles <gyeol3474@gmail.com>
2024-10-13 07:26:19 +0000

    update 2024-10-13

--- java/src/App.java
+++ java/src/App.java
@@ -1,24 +1,44 @@
 import java.util.Scanner;
+import java.util.Stack;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        for(int i=0; i<n; i++){
-            int p = scanner.nextInt();
-            scanner.nextLine();
-            String[] C = scanner.nextLine().split(" ");
-            int cmp = Integer.parseInt(C[0]);
-            String result = C[1];
-            for(int j=2; j<=p; j++){
-                String[] sp = scanner.nextLine().split(" ");
-                if(Integer.parseInt(sp[0])>cmp){
-                    cmp=Integer.parseInt(sp[0]);
-                    result = sp[1];
+        scanner.nextInt();
+        scanner.nextLine();
+
+        String input = scanner.nextLine();
+            int result = 0;
+            Stack<Character> stack = new Stack<>();
+
+            for (int i=0 ; i<input.length() ; i++) {
+                char ch = input.charAt(i);
+                if ('1' <= ch && ch <= '9') {
+                    result++;
+                } else if (ch == 'L' || ch == 'S') {
+                    if (!stack.empty() && stack.peek() != ch) {
+                        stack = new Stack<>();
+                    }
+                    stack.push(ch);
+                } else if (ch == 'R' || ch == 'K') {
+                    if (ch == 'R') {
+                        if (!stack.empty() && stack.peek() == 'L') {
+                            result++;
+                            stack.pop();
+                        } else {
+                            stack = new Stack<>();
+                        }
+                    } else if (ch == 'K') {
+                        if (!stack.empty() && stack.peek() == 'S') {
+                            result++;
+                            stack.pop();
+                        } else {
+                            stack = new Stack<>();
+                        }
+                    }
                 }
             }
             System.out.println(result);
-        }
         scanner.close();
     }
 }
