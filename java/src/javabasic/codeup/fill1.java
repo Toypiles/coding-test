7f3de0c3cc341579d77fce012756a24cc4af927f
Toypiles <gyeol3474@gmail.com>
2025-01-09 13:56:43 +0000

    update codeup

---
+++ java/src/javabasic/codeup/fill1.java
@@ -0,0 +1,23 @@
+package javabasic.codeup;
+
+import java.util.Scanner;
+
+public class fill1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[][] arr = new int[n][n];
+        int k=1;
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[0].length; j++){
+                arr[i][j]=k;
+                k++;
+            }
+        }
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[0].length; j++){
+                System.out.print(arr[i][j]+" ");
+            }System.out.println();
+        }
+    }
+}
--- java/src/javabasic/codeup/sugarCookies.java
+++ java/src/javabasic/codeup/sugarCookies.java
@@ -7,33 +7,25 @@ public class sugarCookies {
         int h= scanner.nextInt();
         int w= scanner.nextInt();
         int n = scanner.nextInt();
-        int[][] arr = new int[11][11];
+        int[][] arr = new int[h+1][w+1];
         for(int t=1; t<=n; t++){
             int l = scanner.nextInt();
             int d = scanner.nextInt();
             int x = scanner.nextInt();
             int y = scanner.nextInt();
-            arr[x][y]=1;
-            for(int i=1; i<=h; i++){
-                for(int j=1; j<=w; j++){
-                    for(int k=0; k<l; k++){
-                        if(d==0){
-                            arr[i][j+k]=1;
-                        }
-                        else if(d==1){
-                            arr[i+k][j]=1;
-                        }
-                    }
+            if(d==0){
+                for(int k=0; k<l; k++){
+                    arr[x][y+k]=1;
+                }
+            } else {
+                for(int k=0; k<l; k++){
+                    arr[x+k][y]=1;
                 }
             }
         }
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=n; j++){
-                if(arr[i][j]==1){
-                    System.out.print(arr[i][j]+" ");
-                }else{
-                    System.out.print(arr[i][j]+" ");
-                }
+        for(int i=1; i<=h; i++){
+            for(int j=1; j<=w; j++){
+                System.out.print(arr[i][j]+" ");
             }System.out.println();
         }
     }
