8ecf99df867488a0a3b113374e1e636a9d76c890
Toypiles <gyeol3474@gmail.com>
2025-01-10 14:11:49 +0000

    update codeup

--- java/src/javabasic/codeup/zigzagfill6.java
+++ java/src/javabasic/codeup/zigzagfill6.java
@@ -1,7 +1,7 @@
 package javabasic.codeup;
 import java.util.Scanner;
 
-public class zigzagfill5 {
+public class zigzagfill6 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
@@ -12,15 +12,14 @@ public class zigzagfill5 {
         for(int i=0; i<arr.length; i++){
             for(int j=0; j<arr[0].length; j++){
                 if(i%2==1){
-                    arr[n-1-i][j]=k;
-                }
-                else{
                     arr[n-1-i][m-1-j]=k;
+                }else{
+                    arr[n-1-i][j]=k;
                 }
                 k++;
             }
         }
-
+        
         for(int i=0; i<arr.length; i++){
             for(int j=0; j<arr[0].length; j++){
                 System.out.print(arr[i][j]+" ");
--- java/src/javabasic/codeup/zigzagfill7.java
+++ java/src/javabasic/codeup/zigzagfill7.java
@@ -1,7 +1,7 @@
 package javabasic.codeup;
 import java.util.Scanner;
 
-public class zigzagfill5 {
+public class zigzagfill7 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
@@ -9,18 +9,17 @@ public class zigzagfill5 {
         int[][] arr = new int[n][m];
         int k=1;
 
-        for(int i=0; i<arr.length; i++){
-            for(int j=0; j<arr[0].length; j++){
-                if(i%2==1){
-                    arr[n-1-i][j]=k;
-                }
-                else{
+        for(int j=0; j<arr[0].length; j++){
+            for(int i=0; i<arr.length; i++){
+                if(j%2==0){
                     arr[n-1-i][m-1-j]=k;
+                }else{
+                    arr[i][m-1-j]=k;
                 }
                 k++;
             }
         }
-
+        
         for(int i=0; i<arr.length; i++){
             for(int j=0; j<arr[0].length; j++){
                 System.out.print(arr[i][j]+" ");
