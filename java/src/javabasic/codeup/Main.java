3edb75871f9960696e1eabf8a7c71166b732df62
Toypiles <gyeol3474@gmail.com>
2025-02-08 11:07:11 +0000

    javabasic study

--- java/src/javabasic/codeup/Main.java
+++ java/src/javabasic/codeup/Main.java
@@ -5,12 +5,24 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner= new Scanner(System.in);
         int n= scanner.nextInt();
-        for(int i=2; i<n; i++){
-            if(n%i==0){
-                System.out.print(i+" ");
+        int m = scanner.nextInt();
+        int[][] arr = new int[n][m];
+        int k=1;
+        for(int j=0; j<arr[0].length; j++){
+            for(int i=0; i<arr.length; i++){
+                if(j%2==0){
+                    arr[n-1-i][m-1-j]=k;
+                }else{
+                    arr[i][m-1-j]=k;
+                }
+                k++;
             }
         }
-        
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[0].length; j++){
+                System.out.print(arr[i][j]+" ");
+            }System.out.println();
+        }
     }
 }
 
