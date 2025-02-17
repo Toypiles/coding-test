cbb17261bf983d7c50074d36687762d72f55955c
Toypiles <gyeol3474@gmail.com>
2025-02-17 11:25:45 +0000

    update

---
+++ java/src/javabasic/baekjoon/baek1874.java
@@ -0,0 +1,43 @@
+package javabasic.baekjoon;
+import java.util.*;
+import java.io.*;
+import java.util.StringTokenizer;
+public class baek1874 {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            int n= scanner.nextInt();
+            int[] arr = new int[n];
+            for(int i=0; i<n; i++){
+                arr[i]=scanner.nextInt();
+            }
+            Stack<Integer> stack = new Stack<>();
+            StringBuffer bf = new StringBuffer();
+            int num=1;
+            boolean result = true;
+            for(int i=0; i<n; i++){
+                int su = arr[i];
+                if(su>=num){
+                    while (su>=num) {
+                        stack.push(num++);
+                        bf.append("+\n");
+                    }
+                    stack.pop();
+                    bf.append("-\n");
+                }
+                else{
+                    int k = stack.pop();
+                    if(k>su){
+                        System.out.println("NO");
+                        result=false;
+                        break;
+                    }
+                    else{
+                        bf.append("-\n");
+                    }
+                }
+            }
+            if(result){
+                System.out.println(bf.toString());
+            }
+        }
+}
