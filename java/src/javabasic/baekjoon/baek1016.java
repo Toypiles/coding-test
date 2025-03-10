8d0edc46a5b140663c0d1472113b08af6c8d06fd
Toypiles <gyeol3474@gmail.com>
2025-03-10 10:59:05 +0000

    update baekjoon solved problem

--- java/src/javabasic/baekjoon/baek1016.java
+++ java/src/javabasic/baekjoon/baek1016.java
@@ -1,10 +1,12 @@
 package javabasic.baekjoon;
-import java.io.*;
-import java.util.*;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-	public static void main(String[] args) throws IOException{
+public class baek1016 {
+    public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		long s = Long.parseLong(st.nextToken());
